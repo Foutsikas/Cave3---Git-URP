3f6e43ee91131a5b4a7e5dc4441c326779373c82
Christos Fotos <c.foutsikas@gmail.com>
2022-12-22 14:31:31 +0200

    New Outline
    Added new outline to objects using shader and a custom renderer.

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField, Range(0,5)] private float maxDistance = 2f;
-    [SerializeField] private Color color;
+    [SerializeField] private Material highlightMaterial;
     private Renderer selectionRenderer;
+    private Material tempMat;
 
     private Transform _selection;
 
     // Update is called once per frame
     void Update()
     {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = tempMat;
+            _selection = null;
+        }
+
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2f, Screen.height/2f, 0f));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, maxDistance,LayerMask.GetMask("Selectable")))
         {
             _selection = hit.transform;
             selectionRenderer = _selection.GetComponent<Renderer>();
+            tempMat = selectionRenderer.material;
             if (selectionRenderer != null)
             {
-                selectionRenderer.material.SetColor("_BaseColor", color);
+                selectionRenderer.material = highlightMaterial;
             }
         }else if (_selection != null)
         {
             selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material.SetColor("_BaseColor", new Color(1, 1, 1, 1));
+            selectionRenderer.material = tempMat;
             _selection = null;
         }
     }
