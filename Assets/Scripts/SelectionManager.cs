3fd7ca9cabdd60e78f9588206b4551060029da0f
Christos Fotos <c.foutsikas@gmail.com>
2022-12-27 15:59:16 +0200

    Object Instatiation
    Added objects to instantiate.
    Added collect button,
    Added collect functions.

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField, Range(0,5)] private float maxDistance = 2f;
+    [SerializeField] private GameObject CollectButton;
     private Renderer selectionRenderer;
 
     private Transform _selection;
@@ -19,15 +20,32 @@ public class SelectionManager : MonoBehaviour
             _selection = hit.transform;
             selectionRenderer = _selection.GetComponent<Renderer>();
             selectionRenderer?.materials[1].SetFloat("_Alpha", 1);
+            CollectButton.SetActive(true);
         }else if (_selection != null)
         {
             selectionRenderer = _selection.GetComponent<Renderer>();
             selectionRenderer.materials[1].SetFloat("_Alpha", 0);
             _selection = null;
+            CollectButton.SetActive(false);
         }
     }
 
     void OnDrawGizmos() {
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward , Color.cyan);
+    }
+
+    public void DestroyThis()
+    {
+        var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2f, Screen.height/2f, 0f));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxDistance, LayerMask.GetMask("Selectable")))
+        {
+            BoxCollider bc = hit.collider as BoxCollider;
+            if (bc != null)
+            {
+                Destroy(bc.gameObject);
+                CollectButton.SetActive(false);
+            }
+        }
     }
 }
