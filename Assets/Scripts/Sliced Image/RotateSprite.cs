6c16845d6459742ce6eb32b0b119d4dfb3076ee4
Christos Fotos <c.foutsikas@gmail.com>
2023-01-17 11:15:26 +0200

    Changes to the Sliced Image Minigame
    -Renamed the scene
    -Now image slices/sprite renderers rotate correctly.
    -Need to fix bug with the sprites not rotating when the game scene loads from the cave scene.

--- Assets/Scripts/Sliced Image/RotateSprite.cs
+++ Assets/Scripts/Sliced Image/RotateSprite.cs
@@ -7,7 +7,7 @@ public class RotateSprite : MonoBehaviour
     public void Rotate()
     {
                 // rotation *= speed;
-        transform.Rotate(Vector3.forward * 90f, Space.World);
+        transform.Rotate(Vector3.forward, -90f, Space.World);
                 // transform.Rotate(-cam.forward, rotation.y + 90f, Space.World);
                 // yield return null;
     }
--- Assets/Scripts/Sliced Image/TouchRotate.cs
+++ Assets/Scripts/Sliced Image/TouchRotate.cs
@@ -4,26 +4,55 @@ using System.Collections;
 
 public class TouchRotate : MonoBehaviour
 {
-    [SerializeField] private InputAction pressed, axis;
+    [SerializeField] private InputAction pressed, axis, screenPos;
+    private Transform cam;
     private Vector2 rotation;
     private bool rotateAllowed;
+    private Vector3 curScreenPos;
+
+    private bool isClickedOn
+	{
+		get
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(curScreenPos);
+                            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+                            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+                            if (hit.collider != null)
+                            {
+                                Debug.Log("Clicked");
+                                return hit.collider.gameObject == this;
+                            }else{
+                                  return false;
+                            }
+		}
+	}
 
     private void Awake()
     {
-        pressed.Enable();
-        axis.Enable();
-        pressed.performed += _ => { StartCoroutine(Rotate()); };
-        pressed.canceled += _ => { rotateAllowed = false; };
-        axis.performed += context => { rotation = context.ReadValue<Vector2>(); };
+        cam = Camera.main.transform;
+        screenPos.performed += context => { curScreenPos = context.ReadValue<Vector2>(); };
+		//pressed.performed += _ => { if(isClickedOn) StartCoroutine(Rotate()); };
+        pressed.performed += _ => { Rotate(); };
+        screenPos.Enable();
+		pressed.Enable();
+		pressed.canceled += _ => { rotateAllowed = false; };
+		axis.performed += context => { rotation = context.ReadValue<Vector2>(); };
+		axis.Enable();
     }
 
-    private IEnumerator Rotate()
-    {
-        rotateAllowed = true;
-        if (rotateAllowed)
-        {
-            transform.Rotate(Vector3.forward, this.rotation.x + 90f);
-            yield return null;
-        }
+
+    private void Rotate(){
+        Debug.Log("Rotating");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(curScreenPos);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero, LayerMask.GetMask("ImageUI"));
+
+        if(hit.collider == null)
+            return;
+        var x = hit.collider.GetComponent<RotateSprite>();
+        if(x)
+         x.Rotate();
     }
 }
