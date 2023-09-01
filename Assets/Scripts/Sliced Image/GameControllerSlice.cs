a4c4b043b0e62a2841311deb25b7f199b9587333
Christos Fotos <47101963+Foutsikas@users.noreply.github.com>
2023-09-01 11:20:27 +0300

    Image Puzzle
    Modified the scripts of the Image puzzle mini game.
    Fixed a bug where on tap the image piece would rotate multiple times instead of just once.
    Made it so that when all the pieces are in the correct orientation, they become noninteractive to allow the player to view the completed image before the win window pops up.

--- Assets/Scripts/Sliced Image/GameControllerSlice.cs
+++ Assets/Scripts/Sliced Image/GameControllerSlice.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 using System.Linq;
 using System.Collections;
+
 public class GameControllerSlice : MonoBehaviour
 {
     [SerializeField] private GameObject[] PicturePieces;
-
     [SerializeField] private GameObject WinPanel;
 
     public static bool youWin = false;
-    private readonly int[] randomZ = new int[] {90, 180, 270};
+    private readonly int[] randomZ = new int[] { 90, 180, 270 };
     private int randomIndex;
+    private bool isRotating = false; // Add a flag to control rotation
 
     void Start()
     {
@@ -19,7 +20,10 @@ public class GameControllerSlice : MonoBehaviour
 
     void Update()
     {
-        RotationCheck();
+        if (!isRotating) // Check if not rotating before allowing rotation check
+        {
+            RotationCheck();
+        }
     }
 
     //Randomizes the rotation of the Z-axis value.
@@ -38,7 +42,22 @@ public class GameControllerSlice : MonoBehaviour
         if (PicturePieces.All(pic => pic.transform.up == Vector3.up))
         {
             youWin = true;
+            isRotating = true; // Lock rotation during the check
+            DisableColliders(); // Disable colliders when the puzzle is solved
             StartCoroutine(Timer());
+        }
+    }
+
+    //Disables the Box Collider 2d Component to allow the user to see the completed image before the "win box" pops up.
+    private void DisableColliders()
+    {
+        foreach (var piece in PicturePieces)
+        {
+            BoxCollider2D collider = piece.GetComponent<BoxCollider2D>();
+            if (collider != null)
+            {
+                collider.enabled = false; // Disable the Box Collider 2D component
+            }
         }
     }
 
--- Assets/Scripts/Sliced Image/TouchRotate.cs
+++ Assets/Scripts/Sliced Image/TouchRotate.cs
@@ -8,33 +8,48 @@ public class TouchRotate : MonoBehaviour
     public Camera GameCamera;
     private Vector2 rotation;
     private Vector3 curScreenPos;
+    private bool isRotating = false; // Add a flag to prevent multiple rotations
 
     private void Awake()
     {
         screenPos.performed += context => { curScreenPos = context.ReadValue<Vector2>(); };
-        pressed.performed += _ => { Rotate(); };
+        pressed.performed += _ => { StartRotation(); };
         screenPos.Enable();
-		pressed.Enable();
-		axis.performed += context => { rotation = context.ReadValue<Vector2>(); };
-		axis.Enable();
+        pressed.Enable();
+        axis.performed += context => { rotation = context.ReadValue<Vector2>(); };
+        axis.Enable();
     }
 
-    private void Rotate(){
+    private void StartRotation()
+    {
+        if (!isRotating)
+        {
+            StartCoroutine(RotateCoroutine());
+        }
+    }
+
+    private IEnumerator RotateCoroutine()
+    {
+        isRotating = true;
         Debug.Log("Rotating1");
         Vector3 mousePos = GameCamera.ScreenToWorldPoint(curScreenPos);
         Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
         RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero, LayerMask.GetMask("ImageUI"));
 
-        if(hit.collider == null)
+        if (hit.collider != null)
         {
-            return;
+            var x = hit.collider.GetComponent<RotateSprite>();
+            if (x)
+            {
+                x.Rotate();
+            }
         }
-        var x = hit.collider.GetComponent<RotateSprite>();
-        if(x)
-        {
-            x.Rotate();
-        }
+
+        // Wait for a short duration to prevent multiple rotations on a single touch
+        yield return new WaitForSeconds(0.2f); // Adjust the duration as needed
+
+        isRotating = false;
     }
 
     private void OnDisable()
